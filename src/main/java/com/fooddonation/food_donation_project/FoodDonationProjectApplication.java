2381edcce41f60d4816d292f074f06e0f73980df
Aditya <adityaankaskar@gmail.com>
2026-06-29 11:37:36 +0530

    Initial project setup

---
+++ src/main/java/com/fooddonation/food_donation_project/FoodDonationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fooddonation.food_donation_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDonationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDonationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fooddonation/food_donation_project/FoodDonationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddonation.food_donation_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDonationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
